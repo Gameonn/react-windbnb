93df35459cc07b1b2d4fc5f3c85de9e4b649e739
Gameonn <jindal.ankit89@gmail.com>
2022-09-15 16:30:23 +0530

    Join multiple classes in Modal component

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+  const cssClasses = [styles.backdrop, props.show ? styles['backdrop-open'] : styles['backdrop-closed']]
+  return <div className={cssClasses.join(' ')} onClick={props.onConfirm} />;
 };
 
 const ModalOverlay = (props) => {
+  const cssClasses = [styles.modal, props.show ? styles['modal-open'] : styles['modal-closed']]
+
   return (
-    <div className={styles.modal}>
+    <div className={cssClasses.join(' ')}>
       <i className="fa fa-close"> </i>
       {props.title && <header className={styles.header}>
         <h5>{props.title}</h5>
@@ -24,9 +27,9 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop show={props.show} onConfirm={props.onConfirm} />, portalElement)}
       {ReactDOM.createPortal(
-        <ModalOverlay title={props.title} onConfirm={props.onConfirm}>
+        <ModalOverlay title={props.title} show={props.show} onConfirm={props.onConfirm}>
           {props.children}
         </ModalOverlay>,
         portalElement
