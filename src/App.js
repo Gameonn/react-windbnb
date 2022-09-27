4970bebde48d9c54fceedc8764235d0211902d8b
Gameonn <jindal.ankit89@gmail.com>
2022-09-27 13:45:50 +0530

    Responsive cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Header from "./Header/Header";
 import Cards from "./Cards/Cards";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [staysCount, setStaysCount] = useState(0);
   // const stays = JSON.parse(localStorage.getItem('stays'));
 
   const staysCounter = (counter) => {
-    console.log('test', counter);
+    console.log("test", counter);
     setStaysCount(counter);
-  }
+  };
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <div className="row gx-5">
+        <div className="row">
           <div className="col">
-          <div className="p-3 text-start"><h5> Stays in Finland </h5></div>
+            <div className="p-3 text-start">
+              <h5> Stays in Finland </h5>
+            </div>
           </div>
           <div className="col">
-            <div className="p-3 text-end"><small><i> {staysCount} stays </i>  </small></div>
+            <div className="p-3 text-end">
+              <small>
+                <i> {staysCount} stays </i>
+              </small>
+            </div>
           </div>
         </div>
         <Cards counter={staysCounter} />
